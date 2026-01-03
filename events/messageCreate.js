80a4ac2484efdcece88efac4c684d436a82afde6
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2026-01-03 21:46:54 +0700

    chore: add pg dependencies

--- events/messageCreate.js
+++
@@ -1,7 +0,0 @@
-export default async function messageCreate(message, client) {
-  if (message.author.bot) return;
-
-  if (message.mentions.has(client.user)) {
-    await message.reply("https://ristek.link/ArsipVille");
-  }
-}
